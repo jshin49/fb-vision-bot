be58a5d2daf0136a71d363ba0817cee83ce03d79
Jay Shin <jshin49@gmail.com>
2016-10-31 15:03:35 +0800

    UPDATE remove unnecessary lines

--- threads.js
+++ threads.js
@@ -9,25 +9,13 @@ const
  *
  */
 
-// App Secret can be retrieved from the App Dashboard
-const APP_SECRET =
-  (process.env.MESSENGER_APP_SECRET) ?
-  process.env.MESSENGER_APP_SECRET :
-  config.get('appSecret');
-
-// Arbitrary value used to validate a webhook
-const VALIDATION_TOKEN =
-  (process.env.MESSENGER_VALIDATION_TOKEN) ?
-  (process.env.MESSENGER_VALIDATION_TOKEN) :
-  config.get('validationToken');
-
 // Generate a page access token for your page from the App Dashboard
 const PAGE_ACCESS_TOKEN =
   (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
   (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
   config.get('pageAccessToken');
 
-if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN)) {
+if (!PAGE_ACCESS_TOKEN) {
   console.error("Missing config values");
   process.exit(1);
 }
